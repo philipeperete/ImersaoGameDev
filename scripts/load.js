74ac04503c28753a377f965ff72cf9903154f94e
Philipe Perete <philipeperete@gmail.com>
2020-06-24 21:36:38 -0300

    Projeto em andamento
    Primeiro commit, projeto em andamento.

---
+++ scripts/load.js
@@ -0,0 +1,14 @@
+function preload() {
+  imagemCenario = loadImage('imagens/cenario/floresta.png');
+  imagemGameOver = loadImage('imagens/assets/game-over.png');
+  imagemPersonagem = loadImage('imagens/personagem/correndo.png');
+  imagemInimigo = loadImage('imagens/inimigos/gotinha.png');
+  imagemInimigoVoador = loadImage('imagens/inimigos/gotinha-voadora.png');
+  imagemInimigoGrande = loadImage('imagens/inimigos/troll.png');
+  imagemTelaInicial = loadImage('imagens/cenario/telaInicial.png');
+  imagemVida = loadImage('imagens/assets/heart.png');
+  fonteTelaInicial = loadFont('imagens/assets/fonteTelaInicial.otf');
+  fita = loadJSON('fita/fita.json')
+  somDoJogo = loadSound('sons/trilha_jogo.mp3');
+  somDoPulo = loadSound('sons/somPulo.mp3');
+}
